0cb9be53791ab9bd89ac9df9d00d0570a14f3098
MaryIshchenko <84804143+MaryIshchenko@users.noreply.github.com>
2022-12-17 23:57:48 -0500

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+export class Home extends Component{
+    render(){
+        return(
+            <div>
+                <h3>Welcome to Suffolk Hospital</h3>
+                <h4> If it's an emergency please dial 911</h4>
+                <h5> For more information or to make an appointment please contact +18567778322</h5>
+            </div>
+            
+        )
+    }
+}
